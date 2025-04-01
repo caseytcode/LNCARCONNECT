33948c3ef23f01d36897d0d48f6394acf0495937
caseytcode <caseytcode@gmail.com>
2025-04-01 02:43:48 +0200

    Update BotpressChat.tsx

--- components/BotpressChat.tsx
+++ components/BotpressChat.tsx
@@ -1,1 +1,43 @@
 
+import { useState } from 'react';
+
+import {
+  Webchat,
+  WebchatProvider,
+  Fab,
+  getClient,
+  Configuration,
+} from '@botpress/webchat';
+
+const clientId = "1a215e37-124f-4b62-840b-52e9a40d60db";
+
+const configuration: Configuration = {
+  color: '#000',
+};
+
+export default function App() {
+  const client = getClient({
+    clientId,
+  });
+
+  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
+
+  const toggleWebchat = () => {
+    setIsWebchatOpen((prevState) => !prevState);
+  };
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WebchatProvider client={client} configuration={configuration}>
+        <Fab onClick={toggleWebchat} />
+        <div
+          style={{
+            display: isWebchatOpen ? 'block' : 'none',
+          }}
+        >
+          <Webchat />
+        </div>
+      </WebchatProvider>
+    </div>
+  );
+}
