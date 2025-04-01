ca94689ab89d9eb9cddc238c3318e48180ad60dc
caseytcode <caseytcode@gmail.com>
2025-04-01 08:14:59 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,38 +12,35 @@ import { Webchat, WebchatProvider, Fab, getClient } from "@botpress/webchat";
 import { buildTheme } from "@botpress/webchat-generator";
 import { useState } from "react";
 
-const { theme, style } = buildTheme({
-  themeName: "prism",
-  themeColor: "#000000",
-});
 
-const clientId = "1a215e37-124f-4b62-840b-52e9a406d0db";
-export default function App() {
-  const client = getClient({ clientId });
-  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
-  const toggleWebchat = () => {
-    setIsWebchatOpen((prevState) => !prevState);
-  };
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <style>{style}</style>
-      <WebchatProvider
-        theme={theme}
-        client={client}
-      >
-        <Fab onClick={toggleWebchat} />
-        <div
-          style={{
-            display: isWebchatOpen ? "block" : "none",
-          }}
-        >
-          <Webchat />
-        </div>
-      </WebchatProvider>
-    </div>
-  );
-}
+const BotpressChat = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.botpress.cloud/webchat/v1/inject.js";
+    script.async = true;
+    document.body.appendChild(script);
 
+    script.onload = () => {
+      window.botpressWebChat.init({
+        "botId": "4d4706d5-110c-4015-a2c1-154ad21d3c32",
+        "clientId": "1a215e37-124f-4b62-840b-52e9a406d0db",
+        "host": "https://cdn.botpress.cloud/webchat/v1",
+        "botName": "Lwandile",
+        "botAvatar": "https://files.bpcontent.cloud/2025/03/23/11/202503231111828-KG9NEE9M.png",
+        "botDescription": "Lwandile is here to help you with buying, selling, and financing cars by providing expert AI assistance.",
+        "color": "#fff",
+        "variant": "solid",
+        "themeMode": "dark",
+        "fontFamily": "rubik",
+        "radius": 0.5,
+        "hideWidget": false,
+        "showCloseButton": true,
+      });
+    };
+  }, []);
+
+  return null;
+};
 
 const fadeIn = {
   hidden: { opacity: 0, y: 20 },
@@ -236,8 +233,7 @@ export default function HomePage() {
         </div>
       </section>
 
-      {/* Add Botpress Chat Component */}
-      <BotpressChat />  {/* This is the Botpress chat component that you added */}
+     <BotpressChat />
 
     </div>
   )
