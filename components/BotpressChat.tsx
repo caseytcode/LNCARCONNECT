c6624c6a8082cad5c628583efb3d6d47268eefda
caseytcode <caseytcode@gmail.com>
2025-04-01 08:09:19 +0200

    Update BotpressChat.tsx

--- components/BotpressChat.tsx
+++ components/BotpressChat.tsx
@@ -1,43 +1,32 @@
+import { useEffect } from "react";
 
-import { useState } from 'react';
+const BotpressChat = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.botpress.cloud/webchat/v1/inject.js";
+    script.async = true;
+    document.body.appendChild(script);
 
-import {
-  Webchat,
-  WebchatProvider,
-  Fab,
-  getClient,
-  Configuration,
-} from '@botpress/webchat';
+    script.onload = () => {
+      window.botpressWebChat.init({
+        "botId": "4d4706d5-110c-4015-a2c1-154ad21d3c32",
+        "clientId": "1a215e37-124f-4b62-840b-52e9a406d0db",
+        "host": "https://cdn.botpress.cloud/webchat/v1",
+        "botName": "Lwandile",
+        "botAvatar": "https://files.bpcontent.cloud/2025/03/23/11/202503231111828-KG9NEE9M.png",
+        "botDescription": "Lwandile is here to help you with buying, selling, and financing cars by providing expert AI assistance.",
+        "color": "#fff",
+        "variant": "solid",
+        "themeMode": "dark",
+        "fontFamily": "rubik",
+        "radius": 0.5,
+        "hideWidget": false,
+        "showCloseButton": true,
+      });
+    };
+  }, []);
 
-const clientId = "1a215e37-124f-4b62-840b-52e9a40d60db";
-
-const configuration: Configuration = {
-  color: '#000',
+  return null; // This component just injects the script, no UI needed
 };
 
-export default function App() {
-  const client = getClient({
-    clientId,
-  });
-
-  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
-
-  const toggleWebchat = () => {
-    setIsWebchatOpen((prevState) => !prevState);
-  };
-
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <WebchatProvider client={client} configuration={configuration}>
-        <Fab onClick={toggleWebchat} />
-        <div
-          style={{
-            display: isWebchatOpen ? 'block' : 'none',
-          }}
-        >
-          <Webchat />
-        </div>
-      </WebchatProvider>
-    </div>
-  );
-}
+export default BotpressChat;
